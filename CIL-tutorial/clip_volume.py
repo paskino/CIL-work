0bbba2c9be90114444918ffce8c0775d28a4f32f
Edoardo Pasca <edo.paskino@gmail.com>
2021-07-14 17:47:40 +0100

    partially working widget

--- CIL-tutorial/clip_volume.py
+++ CIL-tutorial/clip_volume.py
@@ -1,8 +1,10 @@
+import functools
 from cil.io import NEXUSDataReader
 writer = NEXUSDataReader()
 writer.set_up(file_name='noce.nxs')
 recon = writer.read()
 print (recon)
+
 # show2D([data, recon], fix_range=(-0.01,0.06))
 from ccpi.viewer import viewer2D, viewer3D
 from ccpi.viewer.utils.conversion import Converter
@@ -12,27 +14,51 @@ import numpy as np
 
 v  = viewer3D()
 
+def update_clipping_plane(viewer, planew, interactor, event):
+    # print ("Catching ", event)
+    event_translator = planew.GetEventTranslator()
+    pevent = event_translator.GetTranslation(event)
+    print ("pevent", pevent)
+    if pevent =='EndSelect' or pevent == 'Move':
+        rep = planew.GetRepresentation()
+        plane = vtk.vtkPlane()
+        rep.GetPlane(plane)
+        print (plane)
+    
+        v.volume.GetMapper().RemoveAllClippingPlanes()
+        v.volume.GetMapper().AddClippingPlane(plane)
+        #if pevent == vtk.vtkWidgetEvent.Move:
+        v.volume.Modified()
+    # v.getRenderer().Render()
+    # v.updateVolumePipeline()
+
+
 def clipping_plane(v, interactor, event):
     if interactor.GetKeyCode() == "c":
-        print ("handling c")
-        # planew = vtk.vtkImplicitPlaneWidget2()
-        
-        # rep = vtk.vtkImplicitPlaneRepresentation()
-        # planew.SetInteractor(v.getInteractor())
-        # planew.SetRepresentation(rep)
+        if hasattr(v, 'planew'):
+            is_enabled = v.planew.GetEnabled()
+            v.planew.SetEnabled(not is_enabled)
+        else:
+            print ("handling c")
+            planew = vtk.vtkImplicitPlaneWidget2()
+            
+            rep = vtk.vtkImplicitPlaneRepresentation()
+            planew.SetInteractor(v.getInteractor())
+            planew.SetRepresentation(rep)
 
-        plane = vtk.vtkPlane()
-        plane.SetOrigin(0,125,0)
-        plane.SetNormal(0.,1.,0.)
-        # rep.GetPlane(plane)
-        # rep.SetPlaceFactor(1.25)
-        # rep.PlaceWidget(v.volume.GetBounds())
-        v.volume.GetMapper().AddClippingPlane(plane)
-        v.volume.Modified()
-        # planew.On()
-        v.plane = plane
-        # v.planew = planew
-        # planew.AddObserver('EndSelect', lambda: print (plane), 1.0)
+            plane = vtk.vtkPlane()
+            plane.SetOrigin(0,125,0)
+            plane.SetNormal(0.,1.,0.)
+            rep.GetPlane(plane)
+            rep.UpdatePlacement()
+            rep.PlaceWidget(v.volume.GetBounds())
+            v.volume.GetMapper().AddClippingPlane(plane)
+            v.volume.Modified()
+            planew.On()
+            v.plane = plane
+            v.planew = planew
+            v.style.AddObserver('LeftButtonReleaseEvent', functools.partial(update_clipping_plane, v, planew), 1.0)
+
 
 v.style.AddObserver('KeyPressEvent', partial(clipping_plane, v), 1.0)
 
