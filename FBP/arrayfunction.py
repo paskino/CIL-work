a181f3d7265bacd2276b72023b3c3265eab328f2
Edoardo Pasca <edo.paskino@gmail.com>
2021-11-06 00:27:08 +0000

    cleanup

--- FBP/arrayfunction.py
+++ FBP/arrayfunction.py
@@ -3,53 +3,6 @@ import numpy as np
 #https://towardsdatascience.com/wrapping-numpys-arrays-971e015e14bb
 
 
-import functools
-# FIRST
-HANDLED_FUNCTIONS = {}
-
-class Physical():
-    
-    def __init__(self, value, unit=""):
-        self._value = value
-        self._unit = unit
-    
-    # ... other methods here, see above
-       
-    # SECOND
-    def __array_function__(self, func, types, args, kwargs):
-        if func not in HANDLED_FUNCTIONS:
-            return NotImplemented
-        # Note: this allows subclasses that don't override
-        # __array_function__ to handle MyArray objects
-        if not all(issubclass(t, Physical) for t in types):
-            return NotImplemented
-        return HANDLED_FUNCTIONS[func](*args, **kwargs)
-    
-# THIRD
-def implements(numpy_function):
-    """Register an __array_function__ implementation for Physical objects."""
-    def decorator(func):
-        HANDLED_FUNCTIONS[numpy_function] = func
-        return func
-    return decorator
-    
-# FOURTH
-@implements(np.mean)
-def np_mean_for_physical(x, *args, **kwargs):
-    print ("np_mean_for_physical")
-    # first compute the numerical value, with no notion of unit
-    mean_value = np.mean(x._value, *args, **kwargs)
-    # construct a Physical instance with the result, using the same unit
-    return Physical(mean_value, x._unit)
- 
-weights = Physical(np.array([55.6, 45.7, 80.3]), "kilogram")
-heights = Physical(np.array([1.64, 1.85, 1.77]), "meter")
-# bmi = weights/heights**2
-
-print(np.mean(weights)._value, np.mean(weights._value)) # 19.885411834844252 kilogram/meter^2
-
-
-
 HANDLED_FUNCTIONS = {}
 
 def implements(numpy_function):
@@ -89,8 +42,7 @@ class MyArray(object):
     @implements(np.sum)
     def sum(self, *args, **kwargs):
         return sum(self.list)
-    # def __add__(self, other):
-    #     return np.add(self, other)
+
 if __name__ == "__main__":
     A = MyArray([1,2])
     
