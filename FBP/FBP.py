b5c927d621bf3eae2c83e4f61d1859714b46ed60
Edoardo Pasca <edo.paskino@gmail.com>
2021-12-02 12:03:01 +0000

    shuffled things around

--- FBP/FBP.py
+++ FBP/FBP.py
@@ -5,7 +5,8 @@ from cil.framework import AcquisitionGeometry
 from cil.framework import ImageData
 from scipy.fft import fft, ifft, fftfreq
 import numpy as np
-from cil.utilities.display import show2D
+from cil.optimisation.operators import LinearOperator
+
 # from cil.utilities import dataexample
 from PIL import Image
     
@@ -130,58 +131,7 @@ def FBP(data, ig, backend='scipy'):
         
     recon.fill(reconarr)
     return recon
-#%%
 
-
-N = 255
-ig = ImageGeometry(N,N)
-
-ag = AcquisitionGeometry.create_Parallel2D()
-ag.set_panel(N)
-
-angles = np.linspace(0, 360, 100)
-ag.set_angles(angles, angle_unit='degree')
-
-
-#%% Create phantom
-
-ig = ImageGeometry(N,N)
-# TomoPhantom.get_ImageData??
-# phantom = TomoPhantom.get_ImageData(12, ig)
-kernel_size = voxel_num_xy = N
-kernel_radius = (kernel_size ) // 2
-y, x = np.ogrid[-kernel_radius:kernel_radius+1, -kernel_radius:kernel_radius+1]
-
-circle1 = [5,0,0] #r,x,y
-dist1 = ((x - circle1[1])**2 + (y - circle1[2])**2)**0.5
-
-circle2 = [5,80,0] #r,x,y
-dist2 = ((x - circle2[1])**2 + (y - circle2[2])**2)**0.5
-
-circle3 = [25,0,80] #r,x,y
-dist3 = ((x - circle3[1])**2 + (y - circle3[2])**2)**0.5
-
-mask1 =(dist1 - circle1[0]).clip(0,1) 
-mask2 =(dist2 - circle2[0]).clip(0,1) 
-mask3 =(dist3 - circle3[0]).clip(0,1) 
-phantomarr = 1 - np.logical_and(np.logical_and(mask1, mask2),mask3)
-print (phantomarr.shape)
-
-phantom = ImageData(phantomarr, deep_copy=False, geometry=ig, suppress_warning=True)
-show2D(phantom)
-
-# Do something with it
-fwd = FP(phantom, ag)
-data = fwd
-# recon = FBP(data, data.geometry.get_ImageGeometry(), backend='pillow')
-recon = BP(data, data.geometry.get_ImageGeometry())
-show2D([data, recon])
-
-recon2 = FBP(data, phantom.geometry)
-show2D([phantom, recon, recon2], title=['phantom', 'Back Projection', 'fbp'])
-
-#%%
-from cil.optimisation.operators import LinearOperator
 
 class ScipyProjector(LinearOperator):
     def __init__(self, ig, ag):
@@ -199,95 +149,165 @@ class ScipyProjector(LinearOperator):
         else:
             BP(x, self.domain, out=out)
 
-
-from cil.optimisation.algorithms import CGLS
-
-A = ScipyProjector(phantom.geometry, data.geometry)
-
-
-
-algo = CGLS(operator=A, data=data, max_iteration=100)
-algo.run(5)
-
 #%%
-from cil.optimisation.algorithms import PDHG
-from cil.optimisation.functions import BlockFunction, L2NormSquared, MixedL21Norm, IndicatorBox
-from cil.optimisation.operators import GradientOperator, BlockOperator
-alpha_tv = 0.03
-f1 = alpha_tv * MixedL21Norm()
-f2 = L2NormSquared(b=data)
-F = BlockFunction(f1, f2)
 
-# Define BlockOperator K
-Grad = GradientOperator(phantom.geometry)
-K = BlockOperator(Grad, A)
-
-# Define Function G
-G = IndicatorBox(lower=0)
+if __name__ == '__main__':
+    from cil.utilities.display import show2D
 
 
-# Setup and run PDHG
-pdhg_tv_explicit = PDHG(f = F, g = G, operator = K,
-            max_iteration = 1000,
-            update_objective_interval = 200)
-#%%
-pdhg_tv_explicit.run(1000, verbose=1)
-#%%
-show2D([phantom, recon, recon2, algo.solution, pdhg_tv_explicit.solution], \
-    title=['phantom', 'Back Projection', 'fbp', 'CGLS', 'TV'])
+    N = 255
+    ig = ImageGeometry(N,N)
+
+    ag = AcquisitionGeometry.create_Parallel2D()
+    ag.set_panel(N)
+
+    angles = np.linspace(0, 360, 100)
+    ag.set_angles(angles, angle_unit='degree')
 
 
-# dls = dataexample.SYNCHROTRON_PARALLEL_BEAM_DATA.get()
-# print ("##################################### DLS", dls.geometry)
-# data = dls.log()
-# data *= -1
-# data = data.get_slice(vertical='centre')
-# ig = data.geometry.get_ImageGeometry()
-# recon = BP(data, ig)
-# show2D(recon, title=['Back Projection'])
+    #%% Create phantom
 
-# show2D([phantom, recon, recon2, \
-#         data, fwd, fwd - data], \
-#     title=['phantom', 'Back-Projection', 'FBP', \
-#            'ASTRA FWD', 'FP FWD', 'DIFF (FP - ASTRA)_FWD'],\
-#         cmap='gist_earth', num_cols=3)
-# from cil.utilities.display import show_geometry
+    ig = ImageGeometry(N,N)
+    # TomoPhantom.get_ImageData??
+    # phantom = TomoPhantom.get_ImageData(12, ig)
+    kernel_size = voxel_num_xy = N
+    kernel_radius = (kernel_size ) // 2
+    y, x = np.ogrid[-kernel_radius:kernel_radius+1, -kernel_radius:kernel_radius+1]
+
+    circle1 = [5,0,0] #r,x,y
+    dist1 = ((x - circle1[1])**2 + (y - circle1[2])**2)**0.5
+
+    circle2 = [5,80,0] #r,x,y
+    dist2 = ((x - circle2[1])**2 + (y - circle2[2])**2)**0.5
+
+    circle3 = [25,0,80] #r,x,y
+    dist3 = ((x - circle3[1])**2 + (y - circle3[2])**2)**0.5
+
+    mask1 =(dist1 - circle1[0]).clip(0,1) 
+    mask2 =(dist2 - circle2[0]).clip(0,1) 
+    mask3 =(dist3 - circle3[0]).clip(0,1) 
+    phantomarr = 1 - np.logical_and(np.logical_and(mask1, mask2),mask3)
+    print (phantomarr.shape)
+
+    phantom = ImageData(phantomarr, deep_copy=False, geometry=ig, suppress_warning=True)
+    show2D(phantom)
+
+    # Do something with it
+    fwd = FP(phantom, ag)
+    data = fwd
+    # recon = FBP(data, data.geometry.get_ImageGeometry(), backend='pillow')
+    recon = BP(data, data.geometry.get_ImageGeometry())
+    show2D([data, recon])
+
+    recon2 = FBP(data, phantom.geometry)
+    show2D([phantom, recon, recon2], title=['phantom', 'Back Projection', 'fbp'])
+
+    #%%
 
 
-# dls = dataexample.SYNCHROTRON_PARALLEL_BEAM_DATA.get()
-# print ("##################################### DLS", dls.geometry)
-# data = dls.log()
-# data *= -1
-# show_geometry(data.geometry)
-# # dls2d = dls.get_slice(vertical=70)
-# # show2D(dls.get_slice(vertical='centre'))
-# #%%
-# from cil.processors import Slicer, CentreOfRotationCorrector
-# data = CentreOfRotationCorrector.xcorr(slice_index='centre', projection_index=0, ang_tol=0.1)(data)
-# print ("##################################### Centred", data.geometry)
-# # dls2d = data.get_slice(vertical=70)
-# # print ("##################################### Centred", dls2d.geometry)
-# # get centre of rotation
-# cor = data.geometry.config.system.rotation_axis.position
-# px = data.geometry.config.panel.pixel_size[0]
-# cor = [cor[0] * px , cor[1] * px]
-# if cor[0] <= 0:
-#     pad = (0, 2* int(cor[0]))
-# else:
-#     pad = (2* int(cor[0]), -1)
-# print ("CENTRE of ROT", cor, pad)
+    from cil.optimisation.algorithms import CGLS
 
-# from cil.processors import Slicer
-# dls2d = Slicer(roi={'horizontal': pad })(dls.get_slice(vertical=70))
-# data = dls2d.log()
-# data *= -1
-# # dls2d.geometry = dls.get_slice(vertical=70).geometry.copy()
+    A = ScipyProjector(phantom.geometry, data.geometry)
 
-# # # dls2d.geometry.config.system.rotation_axis.position = data.geometry.config.system.rotation_axis.position[:-1]
-# # dls_ig = dls2d.geometry.get_ImageGeometry()
 
-# # print ("rotation axis", data.geometry.config.system.rotation_axis.position)
 
-# # %%
+    algo = CGLS(operator=A, data=data, max_iteration=100)
+    algo.run(5)
 
-# %%
+    #%%
+    from cil.optimisation.algorithms import PDHG
+    from cil.optimisation.functions import BlockFunction, L2NormSquared, MixedL21Norm, IndicatorBox
+    from cil.optimisation.operators import GradientOperator, BlockOperator
+    alpha_tv = 0.03
+    f1 = alpha_tv * MixedL21Norm()
+    f2 = L2NormSquared(b=data)
+    F = BlockFunction(f1, f2)
+
+    # Define BlockOperator K
+    Grad = GradientOperator(phantom.geometry)
+    K = BlockOperator(Grad, A)
+
+    # Define Function G
+    G = IndicatorBox(lower=0)
+
+
+    # Setup and run PDHG
+    pdhg_tv_explicit = PDHG(f = F, g = G, operator = K,
+                max_iteration = 1000,
+                update_objective_interval = 200)
+    #%%
+    pdhg_tv_explicit.max_iteration += 4000
+
+    pdhg_tv_explicit.run(verbose=1)
+    #%%
+    show2D([phantom, recon, recon2, algo.solution, pdhg_tv_explicit.solution], \
+        title=['phantom', 'Back Projection', 'fbp', 'CGLS', 'TV'])
+
+
+    # dls = dataexample.SYNCHROTRON_PARALLEL_BEAM_DATA.get()
+    # print ("##################################### DLS", dls.geometry)
+    # data = dls.log()
+    # data *= -1
+    # data = data.get_slice(vertical='centre')
+    # ig = data.geometry.get_ImageGeometry()
+    # recon = BP(data, ig)
+    # show2D(recon, title=['Back Projection'])
+
+    # show2D([phantom, recon, recon2, \
+    #         data, fwd, fwd - data], \
+    #     title=['phantom', 'Back-Projection', 'FBP', \
+    #            'ASTRA FWD', 'FP FWD', 'DIFF (FP - ASTRA)_FWD'],\
+    #         cmap='gist_earth', num_cols=3)
+    # from cil.utilities.display import show_geometry
+
+
+    # dls = dataexample.SYNCHROTRON_PARALLEL_BEAM_DATA.get()
+    # print ("##################################### DLS", dls.geometry)
+    # data = dls.log()
+    # data *= -1
+    # show_geometry(data.geometry)
+    # # dls2d = dls.get_slice(vertical=70)
+    # # show2D(dls.get_slice(vertical='centre'))
+    # #%%
+    # from cil.processors import Slicer, CentreOfRotationCorrector
+    # data = CentreOfRotationCorrector.xcorr(slice_index='centre', projection_index=0, ang_tol=0.1)(data)
+    # print ("##################################### Centred", data.geometry)
+    # # dls2d = data.get_slice(vertical=70)
+    # # print ("##################################### Centred", dls2d.geometry)
+    # # get centre of rotation
+    # cor = data.geometry.config.system.rotation_axis.position
+    # px = data.geometry.config.panel.pixel_size[0]
+    # cor = [cor[0] * px , cor[1] * px]
+    # if cor[0] <= 0:
+    #     pad = (0, 2* int(cor[0]))
+    # else:
+    #     pad = (2* int(cor[0]), -1)
+    # print ("CENTRE of ROT", cor, pad)
+
+    # from cil.processors import Slicer
+    # dls2d = Slicer(roi={'horizontal': pad })(dls.get_slice(vertical=70))
+    # data = dls2d.log()
+    # data *= -1
+    # # dls2d.geometry = dls.get_slice(vertical=70).geometry.copy()
+
+    # # # dls2d.geometry.config.system.rotation_axis.position = data.geometry.config.system.rotation_axis.position[:-1]
+    # # dls_ig = dls2d.geometry.get_ImageGeometry()
+
+    # # print ("rotation axis", data.geometry.config.system.rotation_axis.position)
+
+    # # %%
+
+    # %%
+    from scipy.sparse.linalg import LinearOperator as sp_LO
+    from functools import partial
+
+    def fw(range, V):
+        return FP(V, range)
+    def bw(domain, V):
+        return BP(V, domain)
+    direct = partial(fw, fwd.geometry.copy())
+    adjoint = partial(bw, phantom.geometry.copy())
+
+    Asp = sp_LO((255,100), matmat=direct, rmatmat=adjoint)
+
+    # %%
