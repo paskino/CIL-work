58edd3b3a37ec798540906c9cd087875fa431c89
Edoardo Pasca <edo.paskino@gmail.com>
2022-03-25 07:58:57 +0000

    added fsspec

--- PSDI/test_fsspec.py
+++ PSDI/test_fsspec.py
@@ -10,8 +10,32 @@ pkey = ssh.RSAKey.from_private_key_file(private_key,password=None)
 conn = {'host':'vishighmem01.esc.rl.ac.uk', 'username':'edo', 'port':22, 'pkey': pkey }
 
 
+# small test file
 g = zarr.open_group('sftp:///mnt/data/edo/Data/PSDI/data/group.zarr', storage_options=conn)
 #%%
 rzdata = np.asarray(g['/bar'])
 
 print (rzdata)
+# %%
+# CT dataset
+ct = zarr.open_group('sftp:///mnt/data/edo/Data/PSDI/scanner_data_bichunks.zarr', 
+                      storage_options=conn)
+
+# %%
+
+from cil.utilities.display import show2D
+import time
+#%%
+
+t0 = time.time()
+slice = ct['/tomodata/array'][100]
+t1 = time.time()
+
+show2D(slice, title="Slice transfer time {:0.3f}s".format(t1-t0))
+# %%
+# access an image on the non chunking axis
+t0 = time.time()
+slice = ct['/tomodata/array'][:,100,:]
+t1 = time.time()
+show2D(slice, title="Reslice transfer time {:0.3f}s".format(t1-t0))
+# %%
