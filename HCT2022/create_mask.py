e0b94720ecc1c4cbb21bec105e77f85590e3ec11
Edoardo Pasca <edo.paskino@gmail.com>
2023-03-24 15:22:06 +0000

    changes

--- HCT2022/create_mask.py
+++ HCT2022/create_mask.py
@@ -11,7 +11,10 @@ from skimage.filters import threshold_otsu
 import matplotlib.pyplot as plt
 from cil.plugins.tigre import ProjectionOperator
 from cil.optimisation.operators import GradientOperator
-from scipy.optimize import minimize
+import matplotlib.pyplot as plt
+import os
+from cil.processors import Slicer
+
 
 
 #%%
@@ -70,7 +73,7 @@ def fit_circle(x,y):
     return (x0,y0,r)
 
 @numba.jit(nopython=True)
-def create_circle(rc, array, value, N, M):
+def fill_circular_mask(rc, array, value, N, M):
 
     for i in numba.prange(M):
         for j in numba.prange(N):
@@ -79,68 +82,9 @@ def create_circle(rc, array, value, N, M):
                 array[i,j] = value
             else:
                 array[i,j] = 0
-
-#%%
-import os
-
-full_data = load_htc2022data(os.path.abspath('C:/Users/ofn77899/Data/HTC2022/htc2022_tb_full.mat'))
-
-# Reduce the dataset 
-# these are indices in the angles array
-# start at offset, use num_angle
-offset = 90
-num_angles = 45
-
-#%%
-from cil.processors import Slicer
-
-roi = {'angle':(offset, offset+num_angles, None)}
-data = Slicer(roi=roi)(full_data)
-
-
-#%%
-
-thresh = threshold_otsu(data.array)
-
-bmask = data.array > thresh
-
-data_mask = data.geometry.allocate(0)
-data_mask.array[bmask] = 1.
-
-#%%
-
-show2D([data, data_mask])
-
-#%%
-ig = data.geometry.get_ImageGeometry()
-A = ProjectionOperator(ig, data.geometry)
-
-# backproject the mask of the data
-backmask = A.adjoint(data_mask)
-show2D(backmask)
-
-#%%
-# find the good edge on the backprojected mask by
-# 1) calculate the gradient magnitude
-# 2) thresholding it with an otsu threshold
-# 3) generate a mask with that threshold
-
-
-grad = GradientOperator(ig)
-mag = grad.direct(backmask)
-mag = mag.get_item(0).power(2) + mag.get_item(1).power(2)
-#%%
-thresh = threshold_otsu(mag.array)
-binary_mask = mag.array > thresh
-mask = mag.geometry.allocate(0)
-mask.array[binary_mask] = 1.
-
-show2D([mag, mask])
-
-#%%
 # find each point x,y in the mask
 @numba.jit(nopython=True)
-def get_points_in_mask(mask, N, M, out, value=1):
+def get_coordinates_in_mask(mask, N, M, out, value=1):
     '''gets the coordinates of the points in a mask'''
     k = 0
     for i in numba.prange(M):
@@ -150,58 +94,141 @@ def get_points_in_mask(mask, N, M, out, value=1):
                 out[1][k] = j
                 k += 1
 
-num_datapoints = np.sum(binary_mask)
-out = np.zeros((2, num_datapoints), dtype=int)
+def calculate_gradient_magnitude(data):
+    '''calculates the magnitude of the gradient of the input data'''
+    grad = GradientOperator(data.geometry)
+    mag = grad.direct(data)
+    mag = mag.get_item(0).power(2) + mag.get_item(1).power(2)
+    return mag
 
-get_points_in_mask(binary_mask, *binary_mask.shape, out)
-x,y = out
-
-
-
-x0,y0,r = fit_circle(x,y)
-
-#%%
 @numba.jit(nopython=True)
-def create_circumference(rc, circle, N, M, rim):
-    '''create a circumference 
-    
-    rc = [radius, x0, y0]
-    circle: data to fill
-    N,M: circle shape
-    rim: size in pixels of the rim of the circumference
-    '''
-    ret = 0
+def set_mask_to_zero(mask, where, where_value, N, M):
     for i in numba.prange(M):
         for j in numba.prange(N):
-            d = np.sqrt( (i-rc[1])*(i-rc[1]) + (j-rc[2])*(j-rc[2]))
-            # if np.isclose(d, rc[0], atol=1e-5):
-            # if np.abs(d - rc[0]) <= (atol + rtol * np.abs(rc[0])):
-            if d < rc[0] and d > rc[0] - rim:
-                circle[i,j] = 1
-            else:
-                circle[i,j] = 0
+            if where[i,j] == where_value:
+                mask[i,j] = 0
+
+def find_circle_parameters(data, ig):
+    '''Creates a circular mask for the reconstruction in the specified ImageGeometry
+    
+    Parameters:
+    -----------
+
+    data: input data, sinogram
+    ig: reconstruction volume geometry
+    value: value to set the mask to, defaults to 1.
+    return_all: boolean to ask to return both the circular mask and the edges
+                of the circle
+
+    Returns:
+    --------
+    ImageData with a circular mask set at value. Optionally returns the edges of
+    the circle if return_all is set to True
+    '''
+
+    # thresh = threshold_otsu(data.array)
+
+    # bmask = data.array > thresh
+
+    # data_mask = data.geometry.allocate(0)
+    # data_mask.array[bmask] = 1.
+
+    ig = data.geometry.get_ImageGeometry()
+    A = ProjectionOperator(ig, data.geometry)
+    recon = FDK(data, ig).run()
+    # recon = A.adjoint(data)
+
+    # find the good edge on the FDK reconstruction
+    # 1) calculate the gradient magnitude
+    # 2) thresholding it with an otsu threshold
+    # 3) generate a mask with that threshold
+
+    mag = calculate_gradient_magnitude(recon)
+    # grad = GradientOperator(ig)
+    # mag = grad.direct(recon)
+    # mag = mag.get_item(0).power(2) + mag.get_item(1).power(2)
+    
+    # initial binary mask
+    thresh = threshold_otsu(mag.array)
+    binary_mask = mag.array > thresh
+
+    mask = ig.allocate(0.)
+    previous_num_datapoints = mask.size
+    num_iterations = 20
+    delta = 4 # pixels
+    value = 1
+    for i in range(num_iterations):
+        
+        maskarr = mask > 0
+
+        set_mask_to_zero(binary_mask, maskarr, value, *binary_mask.shape)
+        
+        # find the coordinates of the points in the binary mask
+        num_datapoints = np.sum(binary_mask)
+        print ("iteration {}, num_datapoints {}, sum(mask) {}".format(i, num_datapoints, np.sum(maskarr)))
+        if num_datapoints < previous_num_datapoints:
+            previous_num_datapoints = num_datapoints
+        else:
+            return np.asarray([r, x0, y0])
+        out = np.zeros((2, num_datapoints), dtype=int)
+
+        get_coordinates_in_mask(binary_mask, *binary_mask.shape, out)
+        # x,y = out
+
+        # fit a circle to the points
+        x0,y0,r = fit_circle(*out)
+
+        # fill a mask and return it
+        mask.fill(0)
+        mycircle = np.asarray([r-delta, x0, y0])
+
+        fill_circular_mask(mycircle, mask.array, value, *mask.shape)
 
 
+
+    
+    return np.asarray([r, x0, y0])
+    
+    
+
+#%%
+
+full_data = load_htc2022data(os.path.abspath('C:/Users/ofn77899/Data/HTC2022/htc2022_tc_full.mat'))
+#%%
+
+
+ig = full_data.geometry.get_ImageGeometry()
+recon_f = FDK(full_data, ig).run()
+#%%
+# Reduce the dataset 
+# these are indices in the angles array
+# start at offset, use num_angle
+offset = 0
+num_angles = 60
+
+#%%
+
+roi = {'angle':(offset, offset+num_angles, None)}
+data = Slicer(roi=roi)(full_data)
+
+
+#%%
+
+ig = data.geometry.get_ImageGeometry()
+recon = FDK(data, ig).run()
+
+#%%
+circle_parameters = find_circle_parameters(data, ig)
+mask = ig.allocate(0)
+fill_circular_mask(circle_parameters, mask.array, 1, *mask.shape)
+
+#%%
+circle = calculate_gradient_magnitude(mask)
+plt.imshow(recon_f.array, cmap='gray_r', vmax = 1,  origin='lower')
+# plt.imshow(mag.array, cmap='gray_r', vmax = 1, alpha=0.2,  origin='lower')
+plt.imshow(circle.array , cmap='gray_r', alpha=0.4, origin='lower')
+plt.show()
 
 # %%
-mycircle = np.asarray([r,x0,y0])
-
-rim = 1
-
-circ = ig.allocate(0)
-create_circumference(mycircle, circ.array, *circ.shape, rim)
-import matplotlib.pyplot as plt
-plt.imshow(mag.array, cmap='inferno', vmax = 200,  origin='lower')
-plt.imshow(circ.array , cmap='gray', alpha=0.4,origin='lower')
-
-plt.show()
-#%%
-new_mask = ig.allocate(0)
-
-create_circle(mycircle, new_mask.array, 1, *new_mask.shape)
-plt.imshow(mag.array, cmap='inferno', vmax = 200,  origin='lower')
-plt.imshow(new_mask.array , cmap='gray', alpha=0.4,origin='lower')
-
-plt.show()
 
 # %%
