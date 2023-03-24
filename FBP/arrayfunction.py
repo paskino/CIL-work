c6779db303f978043d50244345ce35f23c577a55
Edoardo Pasca <edo.paskino@gmail.com>
2023-03-24 15:15:37 +0000

    working NEP18 class

--- FBP/arrayfunction.py
+++ FBP/arrayfunction.py
@@ -1,7 +1,14 @@
 import numpy as np
-import pysnooper
+import logging
+# import torch
+# import pysnooper
 #https://towardsdatascience.com/wrapping-numpys-arrays-971e015e14bb
-
+import inspect
+# initializing function
+def get_function_name():
+    # get the frame object of the function
+    frame = inspect.currentframe()
+    return frame.f_code.co_name
 
 HANDLED_FUNCTIONS = {}
 
@@ -14,17 +21,32 @@ def implements(numpy_function):
 
 class MyArray(object):
     # https://github.com/cupy/cupy/blob/32014aacdb347cf3f22a186cd6e30421bb4a2fe7/cupy/_core/core.pyx#L1581
+    # @pysnooper.snoop()
     def __array_function__(self, func, types, args, kwargs):
+        logging.debug('{} {}'.format(inspect.currentframe().f_code.co_name, func))
         if func not in HANDLED_FUNCTIONS:
             return NotImplemented
         if not all(issubclass(t, MyArray) for t in types):
             return NotImplemented
         return HANDLED_FUNCTIONS[func](*args, **kwargs)
-
-
+    
+    # @pysnooper.snoop()
+    def __array_ufunc__(self, ufunc, method, inputs, *args, **kwargs):
+        logging.debug('{} {}'.format(inspect.currentframe().f_code.co_name, ufunc))
+        if ufunc not in HANDLED_FUNCTIONS:
+            return NotImplemented
+        out = kwargs.pop('out', None)
+    
+        if out is not None:
+            HANDLED_FUNCTIONS[ufunc](inputs, *args, out=out[0], **kwargs)
+            return
+        else:
+            return HANDLED_FUNCTIONS[ufunc](inputs, *args, out=None, **kwargs)
+        
     def __init__(self, ndarray):
         # self.list = np.asarray(ndarray)
         self.list = ndarray[:]
+        # self.__array_ufunc__=None
     
     
     @property
@@ -41,26 +63,6 @@ class MyArray(object):
         return np.dtype(np.int32)
         
     # https://stackoverflow.com/questions/40378427/numpy-formal-definition-of-array-like-objects
-    # @property
-    # def __array_interface__(self):
-    #      return {'shape': self.shape, 
-    #              'typestr': self.dtype.str,
-    #              'data': (id(self.list), False),
-    #              'version': 3}
-    # def __array__(self, dtype=None):
-    #     '''Returns either a new reference to self if dtype is not given or a new array of provided data type 
-    #        if dtype is different from the current dtype of the array.'''
-    #     if dtype is None:
-    #         # a = self
-    #         # return a
-    #         ret = [el for el in self.list]
-    #         return MyArray(ret)
-    #     if dtype != self.dtype:
-    #         ret = [dtype(el) for el in self.list]
-    #         return MyArray(ret)
-            
-    # def len(self):
-    #     return self.shape
     
     def __getitem__(self, x):
         return self.list[x]
@@ -70,20 +72,16 @@ class MyArray(object):
     def __str__(self):
         return "MyArray " + str(self.list)
     
+    def __add__(self, other, *args, **kwargs):
+        logging.debug('{}'.format(inspect.currentframe().f_code.co_name))
+        return self.add(other, *args, **kwargs)
 
-    # apparently calling np.add requires the operator
-    # __add__ to be defined otherwise there is a 
-    # TypeError: unsupported operand type(s) for +: 'MyArray' and 'int'
-    # complaining that we don't know how to make + 
-    # however I am calling np.add not + and @implements(np.add)
-    # is decorating the method below, so why is this at all required??
-    def __add__(self, other):
-        return self.add(other)
-
-    @pysnooper.snoop()
+    # @pysnooper.snoop()
     @implements(np.add)
     def add(self, *args, **kwargs):
-        print ("MyClass add, out", kwargs.get('out', None), len(args))
+        strng = "MyClass add, out {} {}".format( kwargs.get('out', None), len(args) )
+        logging.debug('{} {}'.format(inspect.currentframe().f_code.co_name, strng))
+        # print('Sto chiamando la np.add dentro implements')
         out = kwargs.get('out', None)
         if out is None:
             return MyArray([el + args[0] for el in self.list])
@@ -94,31 +92,53 @@ class MyArray(object):
     # implements np.sum is required when one wants to use the np.sum on this object            
     @implements(np.sum)
     def sum(self, *args, **kwargs):
-        return sum(self.list)
+        return sum(self.list)  # return self.list.ndim
+    
+        
+    def __getitem__(self, x):
+        return self.list[x]
+    def __setitem__(self, key, value):
+        self.list[key] = value
 
+    def __str__(self):
+        return "MyArray " + str(self.list)
+    
 # @pysnooper.snoop()
 def main():
+    logging.basicConfig(level=logging.DEBUG)
 
-    A = MyArray([1,2])
+    A = MyArray(np.array([1,2]))
     
     # test np.sum
-    print ("sum" , np.sum(A))
+    print ("sum" , np.sum(A, axis=1))
     
     # test add
     B = A.add(2)
+    printit(B, 'B')
     
     out = MyArray([20,30])
+    printit(out,'out')
     A.add(2,out=out)
     printit(out,'out')
 
-
     # test np.add
     # see comments on __add__ 
+    #B = A+2
     B = np.add(A,2)
+    printit(B, 'B')
 
-    print (B)
+    print (">>>>>>>>>>>>>")
+    B = A+2
+    printit(B, 'B')
+    print (">>>>>>>>>>>>>")
+
+    # out = MyArray([20,30])
+    np.add(A,2,out=out)
+    printit(out, "out")
     
-    out = MyArray([20,30])
+
+    exit(0)
+
     arr = np.array(out)
     printit(arr, "arr")
     print ("What is arr", type(arr), type(out), arr)
@@ -128,28 +148,28 @@ def main():
     printit(arr, "arr")
     printit(out, "out")
 
-    out[0] = 0
-    printit(arr, "arr")
-    printit(out, "out")
+    # out[0] = 0
+    # printit(arr, "arr")
+    # printit(out, "out")
 
-    # if the out parameter contains a MyArray this call will fail with
-    # TypeError: return arrays must be of ArrayType
-    np.add(A,2,out=out)
-    # having created a numpy array from MyArray, this would mean that the MyArray is an
-    # array like object, no?
-    # Anyway, the out and arr variables seem to point to the same memory area: modifying out 
-    # results in a modification of arr
-    # this line works and the correct output is calculated and stored in arr, however it is not 
-    # anymore linked to out.
+    # # if the out parameter contains a MyArray this call will fail with
+    # # TypeError: return arrays must be of ArrayType
+    # np.add(A,2,out=out)
+    # # having created a numpy array from MyArray, this would mean that the MyArray is an
+    # # array like object, no?
+    # # Anyway, the out and arr variables seem to point to the same memory area: modifying out 
+    # # results in a modification of arr
+    # # this line works and the correct output is calculated and stored in arr, however it is not 
+    # # anymore linked to out.
     np.add(A,2,out=arr)
     
-    print (arr.shape, arr.ndim, arr.dtype,  arr.__array_interface__['data'])
+    # print (arr.shape, arr.ndim, arr.dtype,  arr.__array_interface__['data'])
     printit(arr, "arr")
     printit(out, "out")
 
-    out[0] = -1
-    printit(arr, "arr")
-    printit(out, "out")
+    # out[0] = -1
+    # printit(arr, "arr")
+    # printit(out, "out")
     
     
 
