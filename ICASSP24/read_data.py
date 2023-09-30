08fe9021fd2929a7f02f7f816f1d21d0ff007b9f
Edoardo Pasca <edo.paskino@gmail.com>
2023-09-30 21:38:40 +0000

    add initial inputs, save to json and nxs

--- ICASSP24/read_data.py
+++ ICASSP24/read_data.py
@@ -4,11 +4,36 @@ from cil.framework import ImageGeometry, AcquisitionGeometry, DataOrder
 from cil.utilities.display import show2D, show_geometry
 from cil.framework import AcquisitionData
 from cil.recon import FDK
+from cil.optimisation.functions import TotalVariation, L2NormSquared, WeightedL2NormSquared
+from cil.plugins.tigre import ProjectionOperator
+from cil.optimisation.algorithms import SPDHG, FISTA
+from cil.optimisation.functions import BlockFunction
+from cil.utilities.quality_measures import mse
+from cil.framework import ImageData
+import matplotlib.pyplot as plt
+import json
+import os
+from cil.utilities.display import show1D
+from cil.io import NEXUSDataWriter
+
+
 
 #%%
+# setup
+n_subs = 36
+gamma = 10
+epochs = 20
+alpha = 100
+
+amin = 0
+amax = 1.5
+
+bbox = [150,230, 120,200]
+vertical_slice = 170
+horizontal_y = 180
 
 dose = 'low'
-filename = f'C:/Users/ofn77899/Data/ICASSP24/0000_sino_{dose}_dose.npy'
+filename = f'/opt/data/ICASSP24/train/train/0000_sino_{dose}_dose.npy'
 
 data=np.asarray(np.load(filename,allow_pickle=True), dtype=np.float32)
 
@@ -55,52 +80,106 @@ fdk = FDK(ad, ig).run()
 show2D(fdk)
 # %%
 
-from cil.optimisation.functions import TotalVariation, L2NormSquared, IndicatorBox, LeastSquares
-from cil.plugins.tigre import ProjectionOperator
-from cil.optimisation.algorithms import SPDHG, FISTA
 
 #%% 
 # # Test in 2D
 # data2d = ad.get_slice('centre')
 # ig2d = ig.get_slice('centre')
 
-#%%
-A = ProjectionOperator(ig, AG)
 
-
-F = LeastSquares(A, ad)
 #%%
-alpha = 100
 g = alpha * TotalVariation(max_iteration=5)
 
 #%%
-algo = FISTA(fdk, F, g, max_iteration=100, update_objective_interval=5)
-#%%
-algo.run(10, verbose=1, print_interval=1)
-
-#%%
-show2D([fdk, algo.solution], cmap=['gray', 'gray'])
-# %%
 #SPDHG
-from cil.optimisation.functions import BlockFunction
-n_subs = 36
 sdata = ad.partition(n_subs, 'staggered')
 A = ProjectionOperator(ig, sdata.geometry)
 
 f = [L2NormSquared(b=el) for el in sdata]
 f = BlockFunction(*f)
 # %%
-gammas = [1e-4, 1e-2, 1e-1, 1e1, 1e4]
 
-n_subs = 36
-
-spdhg = SPDHG(f=f, g=g, operator=A, max_iteration=100, update_objective_interval=100,\
-     gamma=gammas[1])
+spdhg = SPDHG(f=f, g=g, operator=A, max_iteration=100 * n_subs,\
+      update_objective_interval=n_subs,\
+      gamma=gamma)
 #%%
-spdhg.run(20 * n_subs, verbose=2, print_interval=2)
+spdhg.run(epochs * n_subs, verbose=2, print_interval=n_subs)
 # %%
+
+#%%
 show2D([fdk, spdhg.solution], cmap=['gray', 'gray'])
 # %%
-import matplotlib.pyplot as plt
-plt.semilogy(spdhg.objective)
+
+plt.semilogy(spdhg.objective[1:])
+plt.show()
+# %%
+
+gt = ImageData(np.load('/opt/data/ICASSP24/train/train/0000_clean_fdk_256.npy'),\
+                         geometry=ig.copy())
+# We will rank by the average MSE on the test set (unreleased)
+dmse = mse(spdhg.solution, gt)
+print (dmse)
+# %%
+# apply circular mask for display
+savedir = os.path.join(os.getcwd(), 'results')
+
+radius = 1
+mfdk = fdk.apply_circular_mask(radius=radius, in_place=False)
+malgo = spdhg.solution.apply_circular_mask(radius=radius, in_place=False)
+mgt = gt.apply_circular_mask(radius=radius, in_place=False)
+
+# amax = max([mfdk.max(), malgo.max(), gt.max()])
+# amin = min([mfdk.min(), malgo.min(), gt.min()])
+diff = mgt-malgo
+lim = max(abs(diff.max()), abs(diff.min()))
+
+# zoom in
+
+show2D([el.get_slice(vertical=vertical_slice)\
+               .as_array()[bbox[0]:bbox[1], bbox[2]:bbox[3]] \
+          for el in [malgo, mfdk, mgt, diff]], \
+     cmap=['viridis', 'viridis', 'viridis', 'seismic'],\
+     title=[ f'SPDHG MSE {dmse}', 'FDK','Ground Truth', 'Difference'],\
+     fix_range=[(amin, amax), (amin, amax), (amin, amax), (-lim/2, lim/2)])
+# %%
+show2D([el.get_slice(vertical=vertical_slice) \
+     for el in [malgo, mfdk, mgt, diff]], \
+     cmap=['viridis', 'viridis', 'viridis', 'seismic'],\
+     title=[ f'SPDHG MSE {dmse}', 'FDK','Ground Truth', 'Difference'],\
+     fix_range=[(amin, amax), (amin, amax), (amin, amax), (-lim/2, lim/2)])
+# %%
+
+show1D([el.get_slice(vertical=vertical_slice) \
+     for el in [malgo, mfdk, mgt]], \
+          label = ['SPDHG', 'FDK', 'Ground Truth'],\
+          slice_list = [('horizontal_y', horizontal_y)], \
+          title=f'Comparison of the reconstruction at y={horizontal_y}')
+
+# %%
+# save 
+
+fname = os.path.join(savedir, f'SPDHG_{n_subs}_{gamma}_{alpha}_{epochs}.json')
+fname2 = os.path.join(savedir, f'SPDHG_{n_subs}_{gamma}_{alpha}_{epochs}.nxs')
+
+
+if not os.path.exists(savedir):
+     os.makedirs(savedir)
+     
+w = NEXUSDataWriter()
+w.set_up(data=spdhg.solution,
+            file_name=fname2)
+w.write()
+
+res = {
+     'n_subs': n_subs,
+     'gamma': gamma,
+     'epochs': epochs ,
+     'alpha': alpha,
+     'mse': dmse,
+     'objective': spdhg.objective,
+     'solution': fname2
+}
+
+with open(fname, 'w') as f:
+     json.dump(res, f)
 # %%
