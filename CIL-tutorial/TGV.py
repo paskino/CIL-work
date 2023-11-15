3c48957f6c5e18ca333e2fb60a5c9897af979efa
Edoardo Pasca <edo.paskino@gmail.com>
2023-11-15 15:12:31 +0000

    add some CVXPY

--- CIL-tutorial/TGV.py
+++ CIL-tutorial/TGV.py
@@ -14,7 +14,10 @@ from cil.io import NEXUSDataReader
 from cil.utilities import dataexample
 from cil.utilities.display import show2D
 from cil.plugins.ccpi_regularisation.functions import TGV
-
+from cil.optimisation.functions import L2NormSquared
+import numpy as np
+import scipy.sparse as sp
+import cvxpy
 #%%
 
 N = 64
@@ -22,12 +25,6 @@ M = N
 data = dataexample.CAMERA.get(size=(N, M))
 
 show2D(data)
-#%%
-tgv = TGV()
-a = tgv.proximal(data, 0.1)
-
-show2D([data, a])
-
 #%%
 # Setup PDHG for TGV regularisation
 alpha = 0.10
@@ -37,9 +34,18 @@ gamma = 1.0
 
 beta = alpha / gamma
 
+#%%
+tgv = TGV(alpha=alpha, gamma=gamma, max_iteration=1000)
+a = tgv.proximal(data, 1.0)
+
+show2D([data, a])
+
+#%%
+
 noisy_data = data
 # Define BlockFunction f
-f1 = L1Norm(b=noisy_data)
+
+f1 = 0.5 * L2NormSquared(b=noisy_data)
 f2 = alpha * MixedL21Norm()
 f3 = beta * MixedL21Norm() 
 f = BlockFunction(f1, f2, f3)         
@@ -62,17 +68,160 @@ K = BlockOperator(K11, K12, K21, -K22, K31, K32, shape=(3,2) )
 
 # Compute operator Norm
 normK = K.norm()
-sigma = 1.
-tau = 1./(sigma*normK**2)
-
+sigma = 1./normK
+tau = 1./normK
+#%%
 # Setup and run the PDHG algorithm
 pdhg = PDHG(f=f,g=g,operator=K,
-            max_iteration = 100, 
+            max_iteration = 1000, 
             # sigma=sigma, tau=tau,
             update_objective_interval = 100)
-pdhg.run(verbose = 2)    
+pdhg.run(1000, verbose = 2, print_interval = 100)    
 
+tv_cil = pdhg.solution[0]
 #%%
 # 
-show2D([a, pdhg.solution[0]])
+show2D([a, pdhg.solution[0], a - pdhg.solution[0]])
+# %%
+
+def tv_cvxpy_regulariser(u, isotropic=True, direction = "forward", boundaries = "Neumann"):
+
+    G = sparse_gradient_matrix(u.shape, direction = direction, order = 1, boundaries = boundaries)   
+
+    DX, DY = G[1], G[0]
+
+    if isotropic:
+        return cvxpy.sum(cvxpy.norm(cvxpy.vstack([DX @ cvxpy.vec(u), DY @ cvxpy.vec(u)]), 2, axis = 0))
+    else:
+        return cvxpy.sum(cvxpy.norm(cvxpy.vstack([DX @ cvxpy.vec(u), DY @ cvxpy.vec(u)]), 1, axis = 0)) 
+
+# Create gradient operator as a sparse matrix that will be used in CVXpy to define Gradient based regularisers
+def sparse_gradient_matrix(shape, direction='forward', order=1, boundaries='Neumann', **kwargs):
+    
+    len_shape = len(shape)    
+    allMat = dict.fromkeys(range(len_shape))
+    discretization = kwargs.get('discretization',[1.0]*len_shape)
+
+    if order == 1:
+
+        # loop over the different directions
+        for i in range(0,len_shape):
+
+            if direction == 'forward':
+            
+                # create a sparse matrix with -1 in the main diagonal and 1 in the 1st diagonal
+                mat = 1/discretization[i] * sp.spdiags(np.vstack([-np.ones((1,shape[i])),np.ones((1,shape[i]))]), [0,1], shape[i], shape[i], format = 'lil')
+
+                # boundary conditions
+                if boundaries == 'Neumann':
+                    mat[-1,:] = 0
+                elif boundaries == 'Periodic':
+                    mat[-1,0] = 1
+
+            elif direction == 'backward':
+
+                # create a sparse matrix with -1 in the -1 and 1 in the main diagonal
+                mat = 1/discretization[i] * sp.spdiags(np.vstack([-np.ones((1,shape[i])),np.ones((1,shape[i]))]), [-1,0], shape[i], shape[i], format = 'lil')
+
+                # boundary conditions
+                if boundaries == 'Neumann':
+                    mat[:,-1] = 0
+                elif boundaries == 'Periodic':
+                    mat[0,-1] = -1
+
+            # Use Kronecker product to compute the full sparse matrix representing the GradientOperator. This will be applied
+            # to a "flatten" array, i.e., a vector and "the reshaped" result describes the forward/backward differences for all
+            # the directions from "len_shape" and under different boundary conditions, e.g., Neumann and Periodic.
+
+            # The difference with the GradientOperator.py and FiniteDifferenceOperator.py is that we do not store in memory a matrix
+            # in order to compute the matrix-vector multiplication "A*x". This is also known as "matrix free" optimisation problem.
+            # However, to set up and use the API of CVXpy, we need this matrix representation of a linear operator such as the GradientOperator.
+            # 
+            # The following constructs the finite complete difference matrix for all (len_shape) dimensions and store the ith finite 
+            # difference matrix in allmat[i].  In 2D we have
+            # allmat[0] = D kron I
+            # allmat[1] = I kron D
+            # and in 3D
+            # allmat[0] = D kron I kron I
+            # allmat[1] = I kron D kron I
+            # allmat[2] = I kron I kron D
+            # and so on, for kron meaning the kronecker product.
+            
+            # For a (n x m) array, the forward difference operator in y-direction (x-direction) (with Neumann/Periodic bc) is a (n*m x n*m) sparse array containing -1,1.
+            # Example, for a 3x3 array U, the forward difference operator in y-direction with Neumann bc is a 9x9 sparse array containing -1,1.
+            # To create this sparse matrix, we first create a "kernel" matrix, shown below:
+            # mat = [-1, 1, 0
+            #        0, -1, 1,
+            #        0, 0, 0].
+            # where the last row is filled with zeros due to the Neumann boundary condition. Then, we use the Kronecker product: allMat[0] = mat x I_m =
+            # matrix([[-1., 1., 0., 0., 0., 0., 0., 0., 0.],
+            #         [ 0., -1., 1., 0., 0., 0., 0., 0., 0.],
+            #         [ 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+            #         [ 0., 0., 0., -1., 1., 0., 0., 0., 0.],
+            #         [ 0., 0., 0., 0., -1., 1., 0., 0., 0.],
+            #         [ 0., 0., 0., 0., 0., 0., 0., 0., 0.],
+            #         [ 0., 0., 0., 0., 0., 0., -1., 1., 0.],
+            #         [ 0., 0., 0., 0., 0., 0., 0., -1., 1.],
+            #         [ 0., 0., 0., 0., 0., 0., 0., 0., 0.]])
+            # where I_m is an (mxm) sparse array with ones in the diagonal.
+            # Then allmat can be applied to a (3x3) array "flatten" columnwise
+            # and represent the forward differences in y direction,i.e.,
+            # [U_{21} - U_{11},
+            #  U_{31} - U_{21},
+            #        0        ,
+            #       ...       ,
+            #       ...       ,
+            #       ...       ,
+            #       ...       ,
+            #       ...       ,
+            #       ...       ]
+            # For the x-direction, we have allmat[1] = I_n x mat.
+
+            # For more details, see "Infimal Convolution Regularizations with Discrete l1-type Functionals, S. Setzer, G. Steidl and T. Teuber"
+        
+            # According to the direction, tmpGrad is either a kernel matrix or sparse eye array, which is updated 
+            # using the kronecker product to derive the sparse matrices.
+            if i==0:
+                tmpGrad = mat
+            else: 
+                tmpGrad = sp.eye(shape[0])
+
+            for j in range(1, len_shape):
+
+                if j == i:
+                    tmpGrad = sp.kron(mat, tmpGrad ) 
+                else:
+                    tmpGrad = sp.kron(sp.eye(shape[j]), tmpGrad )
+
+            allMat[i] = tmpGrad
+
+    else:
+        raise NotImplementedError    
+
+    return allMat        
+
+
+# solution
+u_cvx = cvxpy.Variable(noisy_data.shape)
+
+# fidelity term
+fidelity = 0.5 * cvxpy.sum_squares(u_cvx - noisy_data.array)   
+regulariser = alpha * tv_cvxpy_regulariser(u_cvx)
+
+# objective
+obj =  cvxpy.Minimize( regulariser +  fidelity)
+prob = cvxpy.Problem(obj, constraints = [])
+
+# Choose solver ( SCS, MOSEK(license needed) )
+tv_cvxpy = prob.solve(verbose = True, solver = cvxpy.SCS)
+
+
+import numpy as np
+tv_cil = pdhg.solution[0]
+# compare solution
+#%%
+show2D([tv_cil, u_cvx.value], title=['CIL', 'CVXPY'])
+#%%
+np.testing.assert_allclose(tv_cil.array, u_cvx.value,atol=1e-3)  
+
 # %%
